0f1a681284a568321e8299deb24f79f9ab2a72fc
PKT <rudxorrmrwkd@gmail.com>
2023-04-06 23:09:08 +0900

    엑셀 파싱할때 한셀 안돼고/xlsx로 하는게 적용됨

---
+++ Assets/Assets/TextReader.cs
@@ -0,0 +1,13 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TextReader 
+{
+    public int branch;
+    public string Name;
+    public string Dialog;
+    
+}
---
+++ Assets/Assets/TextTestM.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset]
+public class TextTestM : ScriptableObject
+{
+	public List<TextReader> Sheet1; // Replace 'EntityType' to an actual type that is serializable.
+}
